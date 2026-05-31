e581d4e2b4708d074ef8697aba354f688e0bb911
Iklil12 <iklilaquarition96@gmail.com>
2026-05-31 19:00:29 +0700

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: 'deobqjna7',
+    NEXT_PUBLIC_CLOUDINARY_PRESET: 'paperions_preset',
+    NEXT_PUBLIC_BUNNY_LIBRARY_ID: '656801',
+    NEXT_PUBLIC_BUNNY_PULL_ZONE: 'vz-eed0251b-ae9.b-cdn.net',
+  },
 };
 
 export default nextConfig;
